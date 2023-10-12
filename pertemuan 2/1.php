a3462d57572ae86e542913dbc1e5b02c10937aaa
bilalAn-nur <114582087+bilalAn-nur@users.noreply.github.com>
2023-10-12 07:52:21 +0700

    Update 1.php

--- pertemuan 2/1.php
+++ pertemuan 2/1.php
@@ -1,1 +1,12 @@
-test
+<html>
+  <head>
+    <title>Coba</title>
+  </head>
+  <body>
+    <?php 
+      for($i=1;$i<=50;i++)
+        if (i % 2!=0)
+          echo "$i<br>";
+      ?>
+  </body>
+</html>
